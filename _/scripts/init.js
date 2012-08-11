5e0d86d13b1dc10ffd8d8d9e7a886fa59762e434
tomsturge <tomsturge@googlemail.com>
2012-08-11 13:53:56 +0100

    Add social icons

--- _/scripts/init.js
+++ _/scripts/init.js
@@ -1,34 +1,13 @@
 $(document).ready(function() {
 
-  $(".lettering").lettering();
-
   $('.curtains').curtain({
     scrollSpeed: 450,
     controls: '.menu',
     curtainLinks: '.curtain-links'
   });
 
-  var nav = 'nav ul';
-  
-/* 
-  $(nav + ' a').click(function() {
-    $(nav).children().removeClass('active');
-    $(this).parent().addClass('active');
-  });
-*/
-
   $('.introBox').cycle({
     fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
-  });
-
-  $('header h2').cycle ({
-    timeout: 6000,
-    before: function(){
-      $(this).parent().find('a.current').removeClass('current');
-    },
-    after: function(){
-      $(this).addClass('current');
-    }
   });
 
 });
