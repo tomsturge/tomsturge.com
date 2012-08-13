11fff0ac46c43f5ba3d4ec5c254a90b6d452b75b
Thomas Sturge <tomsturge@googlemail.com>
2012-08-13 01:22:32 +0100

    Add functionality to 'Work' section boxes

--- _/scripts/init.js
+++ _/scripts/init.js
@@ -3,12 +3,22 @@ $(document).ready(function() {
   $('.curtains').curtain({
     scrollSpeed: 450,
     controls: '.menu',
-    curtainLinks: '.curtain-links'
+    curtainLinks: '.curtain-links',
+    enableKeys: false
   });
 
   $('.introBox').cycle({
     fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
   });
-
+  
+  var workBox = '.workShowcase li'
+  
+  $(workBox).hover(function (){
+    $(this).children("div").stop().fadeIn('fast');
+  },
+  function (){
+    $(this).children("div").stop().fadeOut('fast');
+  });
+    
 });
 
