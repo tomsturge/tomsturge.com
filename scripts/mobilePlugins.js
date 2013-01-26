b2d6a4367eed62d2c7c9dc19c833a6045b339aa4
tomsturge <tomsturge@googlemail.com>
2013-01-26 13:18:07 +0000

    Update scripts/mobilePlugins.js

--- scripts/mobilePlugins.js
+++ scripts/mobilePlugins.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-
+  
+  window.addEventListener("load",function() {
+    // Set a timeout...
+    setTimeout(function(){
+      // Hide the address bar!
+      window.scrollTo(0, 1);
+    }, 0);
+  });
+  
   var workBox = '.slide'
 
   $(workBox).click(function (){
