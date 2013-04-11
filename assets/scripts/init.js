1ba4f98ac2d65b2476ac956d0dda1e1fa502b081
Thomas Sturge <tomsturge@googlemail.com>
2013-04-11 22:52:10 +0100

    Replace CamelCase with hyphen-case element naming scheme
    Signed-off-by: Thomas Sturge <tomsturge@googlemail.com>

--- assets/scripts/init.js
+++ assets/scripts/init.js
@@ -1,16 +1,22 @@
 $(document).ready(function() {
-
-  $('.introBox').cycle({
-    fx: 'fade'
-  });
-
+  
+  var workBox = '.slide'
 
   Modernizr.load([{
-    test: 1025 > window.screen.width
-  , yep: "assets/scripts/mobilePlugins.js"
-  , nope: "assets/scripts/desktopPlugins.js"
+    test: 1025 > window.screen.width,
+    yep: 
+      $(workBox).click(function (){
+        $(this).find(".hoverBox").stop().fadeIn('fast').delay(5000).fadeOut('fast');
+      });,
+    nope: 
+      $(workBox).hover(function() {
+        $(this).find(".hoverBox").stop().fadeIn('fast');
+      }, function() {
+        $(this).find(".hoverBox").stop().fadeOut('fast');
+      });
   }]);
-
+  
+  /*
   $('.workSlider').iosSlider({
     snapToChildren: true,
     scrollbar: true,
@@ -23,6 +29,7 @@ $(document).ready(function() {
     scrollbarBorderRadius: '0',
     scrollbarOpacity: '1'
   });
+  */
 
 });
 
