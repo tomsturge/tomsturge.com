270543bd5dd91ab52b37d93fd63b9bc4758172aa
Thomas Sturge <tomsturge+github@gmail.com>
2014-11-13 10:43:26 +0000

    Modifying nav behaviour

--- assets/js/init.js
+++ assets/js/init.js
@@ -20,9 +20,6 @@ $(window).scroll(function(){
     var scrollTop = $(window).scrollTop();
     var isTop = (scrollTop > 25);
 
-    $('.main-nav').css('background-color',
-                      (isTop) ? 'rgba(0,0,0,.2)' : 'rgba(0,0,0,.65)');
-
     if(isTop){ $('.main-nav').addClass('notTop'); } else { $('.main-nav').removeClass('notTop'); }
 
 
