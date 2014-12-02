70283f9a624b3bc460ea823a7906ef35d000b96b
Thomas Sturge <tomsturge+github@gmail.com>
2014-12-02 01:20:12 +0000

    Fix RequireJS dependancies

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 require.config({
-    "baseUrl": "assets/js",
+    "baseUrl": "/assets/js",
     "paths": {
       "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min"
     }
 });
 
 define(["jquery"], function($) {
-    require(["helper/global.min"])
+    require(["/assets/js/helper/global.min.js"])
 });
