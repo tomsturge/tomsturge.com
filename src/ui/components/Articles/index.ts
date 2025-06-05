64b80dec7fff135f2e945ad0950c5b95da0f5524
Tom Sturge <tom@mohtas.media>
2025-06-05 16:36:30 +0100

    Update topics page

--- src/ui/components/Articles/index.ts
+++ src/ui/components/Articles/index.ts
@@ -1,6 +1,7 @@
 export { CategoryLabel } from "./CategoryLabel";
 export { default as ArticleItem } from "./ArticleItem.astro";
 export { default as ArticleList } from "./ArticleList.astro";
+export { default as RecentList } from "./RecentList.astro";
 export { default as Header } from "./Header.astro";
 export { FeaturedList } from "./FeaturedList";
 export { PortableText } from "./PortableText";
