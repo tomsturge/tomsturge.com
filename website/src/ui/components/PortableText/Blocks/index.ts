f61d2915b5b33804500b348b6d02696b11113e55
Tom Sturge <tom@mohtas.media>
2025-07-11 13:15:02 +0100

    Fix import typo

--- website/src/ui/components/PortableText/Blocks/index.ts
+++ website/src/ui/components/PortableText/Blocks/index.ts
@@ -1,4 +1,4 @@
-export { default as Blockquote } from "./Blockquote.astro";
+export { default as Blockquote } from "./BlockQuote.astro";
 export { default as Divider } from "./Divider.astro";
 export { default as Image } from "./Image.astro";
 export { default as HeadingTwo } from "./HeadingTwo.astro";
